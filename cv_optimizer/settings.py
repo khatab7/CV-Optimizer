50421049d657b8a7c6adb71357c1431ef060246b
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 <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>
2024-09-26 01:55:47 +0200

    add requirements.txt

--- cv_optimizer/settings.py
+++ cv_optimizer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0t&jdj%6w04(j77!7key9kk@$%h_fdav*56w&5t7zx^6p*m^pv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,9 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
